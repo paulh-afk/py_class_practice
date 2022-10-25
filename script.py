993a127fb66d905d0e5c5b8ec3f10021d38e16b4
Paul <paul.helleu@orange.fr>
2022-10-25 20:36:43 +0200

    friends_list

--- script.py
+++ script.py
@@ -4,12 +4,27 @@ class Dog:
         self.breed = input_breed
         self.age = input_age
         self.is_friendly = input_friendliness
+        self.friends = []
 
     def have_birthday(self):
         self.age += 1
         print(self.name, 'is now', self.age, 'yo!')
 
+    def add_friend(self, dog):
+        if dog.is_friendly:
+            self.friends.append(dog)
+
+    def friends_list(self):
+        for dog in self.friends:
+            print(dog.name, 'is a friend of', self.name,
+                  'and he is', self.age, 'yo')
+
 
 dog_one = Dog('Medor', 'Golden Retriever', 9, True)
+dog_two = Dog('Huck', 'Akita', 2, True)
+dog_three = Dog('Tik', 'Australian Shepherd', 2, True)
 
-dog_one.have_birthday()
+dog_one.add_friend(dog_two)
+dog_one.add_friend(dog_three)
+
+dog_one.friends_list()
